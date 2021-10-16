48dcc6bf5dabda484def68fc9ad9d0d7b2f06af0
Nidhi <2019nidhi.thakkar@ves.ac.in>
2021-10-16 22:56:47 +0530

    login signup alerts

--- login.php
+++ login.php
@@ -3,11 +3,11 @@ $login = false;
 $showError = false;
 if($_SERVER["REQUEST_METHOD"] == "POST"){
     include 'DataBase/connection.php';
-    $username = $_POST["email"];
+    $email = $_POST["email"];
     $password = $_POST["password"]; 
     
      
-    $sql = "Select * from user where email='$email' AND password='$password'";
+    $sql = "Select * from `user` where email='$email' AND password='$password'";
     $result = mysqli_query($conn, $sql);
     $num = mysqli_num_rows($result);
     if ($num == 1){
@@ -15,7 +15,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         session_start();
         $_SESSION['loggedin'] = true;
         $_SESSION['email'] = $email;
-        header("location: AdminHome.php");
+        header("location: ./AdminHome.html");
 
     } 
     else{
@@ -25,7 +25,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     
 ?>
 
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
   <head>
     <!-- Required meta tags -->
@@ -34,12 +34,12 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+    <!-- <link rel="stylesheet" href="./style/style.css"> -->
     <title>Login</title>
   </head>
   <body>
-    <?php require 'partials/_nav.php' ?>
-    <?php
+  <?php
     if($login){
     echo ' <div class="alert alert-success alert-dismissible fade show" role="alert">
         <strong>Success!</strong> You are logged in
@@ -57,27 +57,42 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     </div> ';
     }
     ?>
-
-    <div class="container my-4">
-     <h1 class="text-center">Login to our website</h1>
-     <form action="/loginsystem/login.php" method="post">
-        <div class="form-group">
-            <label for="username">Username</label>
-            <input type="text" class="form-control" id="username" name="username" aria-describedby="emailHelp">
-            
+  <!-- login form -->
+  
+  <section id="contact">
+    <div class="container-lg">
+      
+      <div class="text-center">
+        <h2 class="mt-5">Login</h2>
+        <p class="lead">Are you new here? <a href="./signup.php">Signup here</a></p>
+      </div>
+      <div class="row justify-content-center my-5">
+        <div class="col-lg-6">
+          
+          <form action="./login.php" method="post">
+            <label for="email" class="form-label">Email address:</label>
+            <div class="input-group mb-4">
+              <span class="input-group-text">
+                <i class="bi bi-envelope-fill text-secondary"></i>
+              </span>
+              <input type="text" name="email" id="email" class="form-control" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
+                    oninvalid="setCustomValidity('Please enter your email id correctly. ')" placeholder="e.g. email@example.com" />
+            </div>
+            <label for="password" class="form-label">Password</label>
+            <div class="mb-4 input-group">
+              <span class="input-group-text">
+                <i class="bi bi-key-fill text-secondary"></i>
+              </span>
+              <input type="password" name="password" class="form-control" id="password"placeholder="Enter your password">
+            </div>
+            <button type="submit" class="btn btn-secondary">Submit</button>
+            </div>
+          </form>
         </div>
-        <div class="form-group">
-            <label for="password">Password</label>
-            <input type="password" class="form-control" id="password" name="password">
-        </div>
-       
-         
-        <button type="submit" class="btn btn-primary">Login</button>
-     </form>
+      </div>
     </div>
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  </section>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
--- signup.php
+++ signup.php
@@ -1,5 +1,4 @@
 <?php
-$showAlert = false;
 $showError = false;
 if($_SERVER["REQUEST_METHOD"] == "POST"){
     include './DataBase/connection.php';
@@ -9,45 +8,27 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     $phone = $_POST["phone"];
     $password = $_POST["password"];
     $cpassword = $_POST["cpassword"];
-    $exists=false;
-    if(($password == $cpassword) && $exists==false){
-        $sql = "INSERT INTO `user` (`name`, `email`, `address`, `phone`, `password`,`profile`) VALUES ('$name', '$email', '$address', '$phone', '$password', 'C:\\xampp\\htdocs\\Paws and Tails\\assets\\user.png')";
+    $already= "Select * from `user` where phone='$phone'" ;
+    $already1= "Select * from `user` where email='$email'" ;
+    
+    if(($password == $cpassword) && (!$already) && (!$already1)) {
+        $sql = "INSERT INTO `user` (`name`, `email`, `address`, `phone`, `password`) VALUES ('$name', '$email', '$address', '$phone', '$password')";
         $result = mysqli_query($conn, $sql);
         if ($result){
             $showAlert = true;
         }
+        header("location: ./AdminHome.html");
     }
-    else{
+    elseif($already1){
+      $showError = "email id already registered, please login";
+    }
+    elseif($already){
+      $showError = "phone number already registered";
+    }
+    elseif($password!=$cpassword){
         $showError = "Passwords do not match";
     }
-}
-    
-?>
-<?php
-$login = false;
-$showError = false;
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-    include './Database/connection.php';
-    $username = $_POST["email"];
-    $password = $_POST["password"]; 
-    
-     
-    $sql = "Select * from `user` where email='$email' AND password='$password'";
-    $result = mysqli_query($conn, $sql);
-    $num = mysqli_num_rows($result);
-    if ($num == 1){
-        $login = true;
-        session_start();
-        $_SESSION['loggedin'] = true;
-        $_SESSION['email'] = $email;
-        header("location: AdminHome.html");
-
-    } 
-    else{
-        $showError = "Invalid Credentials";
-    }
-}
-    
+}  
 ?>
 
 <!DOCTYPE html>
@@ -57,7 +38,9 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Paws and Tails | Login/Signup</title>
-    <link rel="stylesheet" href="./style/style.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <!-- <link rel="stylesheet" href="./style/style.css" /> -->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link
@@ -68,154 +51,108 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
       href="https://fonts.googleapis.com/css2?family=PT+Sans+Narrow&display=swap"
       rel="stylesheet"
     />
-    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-uWxY/CJNBR+1zjPWmfnSnVxwRheevXITnMqoEIeG1LJrdI0GlVs/9cVSyPYXdcSF" crossorigin="anonymous"> -->
+    <style>
+    section {
+      padding: 60px 0;
+    }
+    #welcome {
+  text-align: center;
+  color: black;
+  text-shadow: 2px 2px 2px rgb(104, 102, 107);
+  padding-top: 5%;
+  font-family: "Gochi Hand", cursive;
+  font-size: 40px;
+}
+  </style>
   </head>
 
   <body>
-  
-    <div class="contain">
+  <?php
+   if($showError){
+    echo ' <div class="alert alert-danger alert-dismissible fade show" role="alert">
+        <strong>Error!</strong> '. $showError.'
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">×</span>
+        </button>
+    </div> ';
+    }
+  ?>
       <h1 id="welcome">Welcome to Paws and Tails!</h1>
-      <div class="wrapper">
-        <div class="login">
-          <h1>Login</h1>
-          <form action="./login.php" method="post">
-            <div class="row">
-              <div class="col-25">
-                <label for="email">Email id</label>
-              </div>
-              <div class="col-60">
-                <input
-                  type="email"
-                  id="email"
-                  name="email"
-                  placeholder="Enter your email"
-                  pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
-                  oninvalid="setCustomValidity('Please enter your email id correctly. ')"
-                /><br />
-              </div>
+      <section id="contact">
+    <div class="container-lg">
+
+      <div class="text-center">
+        <h2>Sign Up</h2>
+        <p class="lead">Already have an account? <a href="./login.php">Login here</a> </p>
+      </div>
+      <div class="row justify-content-center my-5">
+        <div class="col-lg-6">
+
+          <form action="./signup.php" method="post">
+            <label for="name" class="form-label">Full Name:</label>
+            <div class="mb-4 input-group">
+              <span class="input-group-text">
+                <i class="bi bi-person-fill text-secondary"></i>
+              </span>
+              <input required type="text" id="name" name="name" class="form-control" placeholder="Firstname Surname "
+                pattern="[a-zA-Z]+ [a-zA-Z]+$"
+                oninvalid="setCustomValidity('Please enter alphabets only and match the requested format. ')" />
             </div>
-            <div class="row">
-              <div class="col-25">
-                <label for="password">Password</label>
-              </div>
-              <div class="col-60">
-                <input
-                  type="password"
-                  id="password"
-                  name="password"
-                  placeholder="Enter your password"
-                /><br />
-              </div>
+            <label required for="email" class="form-label">Email address:</label>
+            <div class="input-group mb-4">
+              <span class="input-group-text">
+                <i class="bi bi-envelope-fill text-secondary"></i>
+              </span>
+              <input required type="text" id="email" name="email" class="form-control" placeholder="e.g. email@example.com"
+                pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
+                oninvalid="setCustomValidity('Please enter your email id correctly. ')" />
             </div>
-            <br /><br />
-            
-            <div class="row">
-              <input type="submit" value="Login" />
+            <label for="phone" class="form-label">Phone Number</label>
+            <div class="mb-4 input-group">
+              <span class="input-group-text">
+                <i class="bi bi-telephone-fill text-secondary"></i>
+              </span>
+              <input required class="form-control" type="text" id="phone" name="phone" placeholder="eg. 9652XXXXX0"
+                maxlength="10" minlength="10">
             </div>
-          </form>
+            <label for="password" class="form-label">Password</label>
+            <div class="mb-4 input-group">
+              <span class="input-group-text">
+                <i class="bi bi-key-fill text-secondary"></i>
+              </span>
+              <input required type="password" class="form-control" id="password" name="password"
+                placeholder="Choose your password">
+            </div>
+            <label for="password" class="form-label">Confirm Password</label>
+            <div class="mb-4 input-group">
+              <span class="input-group-text">
+                <i class="bi bi-check-circle-fill text-secondary"></i>
+              </span>
+              <input required type="password" class="form-control" id="cpassword" name="cpassword"
+                placeholder="Re-type the password">
+            </div>
+            <label for="address" class="form-label">Address:</label>
+            <div class="input-group mb-4">
+              <span class="input-group-text">
+                <i class="bi bi-house-fill text-secondary"></i>
+              </span>
+              <textarea required class="form-control" name="address" id="address" rows="4"></textarea>
+
+            </div>
+            <div class="mb-4 text-center">
+          <button type="submit" class="btn btn-secondary">Create an Account</button>
+        </div>
         </div>
 
-        <div class="vl"></div>
-        <div class="signup">
-          <h1>Sign Up</h1>
-          <div class="container">
-            <form action="signup.php" method="post">
-              <div class="row">
-                <div class="col-25">
-                  <label for="text">Name</label>
-                </div>
-                <div class="col-60">
-                  <input
-                    type="text"
-                    id="name"
-                    name="name"
-                    placeholder="firstname lastname"
-                    pattern="[a-zA-Z]+ [a-zA-Z]+$"
-                    oninvalid="setCustomValidity('Please enter on alphabets only and match the requested format. ')"
-                  /><br />
-                </div>
-              </div>
-              <div class="row">
-                <div class="col-25">
-                  <label for="email">Email id</label>
-                </div>
-                <div class="col-60">
-                  <input
-                    type="email"
-                    id="email"
-                    name="email"
-                    placeholder="Enter your email"
-                    pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
-                    oninvalid="setCustomValidity('Please enter your email id correctly. ')"
-                  /><br />
-                </div>
-              </div>
-              <div class="row">
-                <div class="col-25">
-                  <label for="password">Password</label>
-                </div>
-                <div class="col-60">
-                  <input
-                    type="password"
-                    id="password"
-                    name="password"
-                    placeholder="Choose your password"
-                  /><br />
-                </div>
-              </div>
-              <div class="row">
-              <div class="col-25">
-                <label for="cpassword">Confirm Password</label>
-              </div>
-              <div class="col-60">
-                <input
-                  type="password"
-                  id="cpassword"
-                  name="cpassword"
-                  placeholder="Confirm Password"
-                /><br />
-              </div>
-            </div>
-            
-              <div class="row">
-                <div class="col-25">
-                  <label for="Phone Number">Phone Number</label>
-                </div>
-                <div class="col-60">
-                  <input
-                    type="text"
-                    id="phone"
-                    name="phone"
-                    placeholder="eg. 9652XXXXX0"
-                    maxlength="10"
-                    minlength="10"
-                  /><br />
-                </div>
-              </div>
-              <div class="row">
-                <div class="col-25">
-                  <label for="Address">Address</label>
-                </div>
-                <div class="col-60">
-                  <textarea
-                    name="address"
-                    id="address"
-                    cols="21"
-                    rows="5"
-                  ></textarea>
-                  <br /><br />
-                </div>
-              </div>
+        
 
-              <br />
-              <div class="row">
-                <input type="submit" value="Sign Up" />
-              </div>
-              
-            </form>
-          </div>
-        </div>
+        </form>
       </div>
     </div>
+    </div>
+  </section>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
   </body>
 </html>
